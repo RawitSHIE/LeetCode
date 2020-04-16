8ea1e1d005c77e1d960c0c711948a94fd60474cf
RawitSHIE <Rawitgun@gmail.com>
2020-04-16 18:35:57 +0700

    added teenode

---
+++ src/medium/DeepestLeavesSum.java
@@ -0,0 +1,28 @@
+package medium;
+
+public class DeepestLeavesSum {
+//    int maxlevel = 0;
+//    public int deepestLeavesSum(TreeNode root) {
+//        return recur(root, 0);
+//    }
+//
+//    public void recur (TreeNode tree, int level) {
+//        if (tree.left == null && tree.right == null) {
+//            if (maxlevel < level) {
+//                maxlevel = level;
+//                return tree.val;
+//            } else if (maxlevel == level){
+//
+//            }
+//        } else if (tree.right != null && tree.left == null) {
+//            recur(tree.right, level + 1);
+//            return;
+//        } else if (tree.right == null) {
+//            recur(tree.left, level + 1);
+//            return;
+//        } else {
+//            recur(tree.left, level + 1) + recur(tree.right, level + 1);
+//            return;
+//        }
+//    }
+}
